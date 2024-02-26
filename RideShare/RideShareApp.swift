401aa2bf160c1889d4645ed05a3b3e1b9d2c05a5
pabluchenko <jp.junco@uniandes.edu.co>
2024-02-26 17:56:15 -0500

    Initial Commit

---
+++ RideShare/RideShareApp.swift
@@ -0,0 +1,17 @@
+//
+//  RideShareApp.swift
+//  RideShare
+//
+//  Created by Pablo Junco on 26/02/24.
+//
+
+import SwiftUI
+
+@main
+struct RideShareApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
