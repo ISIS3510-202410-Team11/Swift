13356dff0939d54c1bacc1ce801d78494f8e6446
Juan Sebastian Pedraza Perez <98646138+JuanSe2003@users.noreply.github.com>
2024-03-21 20:56:33 -0500

    Cambios en profile

---
+++ RideShare/Models/UserModel.swift
@@ -0,0 +1,11 @@
+
+
+import Foundation
+import UIKit
+struct UserModel {
+    var name: String
+    var rating: Double
+    var cedula: Int
+    var paymentMethod: String
+    var profileImage: UIImage?
+}
